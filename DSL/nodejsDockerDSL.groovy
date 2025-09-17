56bf11872d80a58b7ab876e7ea1da66dd89ec9d6
zodd2698 <hellknife2698@gmail.com>
2025-09-17 20:38:56 +0000

    Fixing Docker repo

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('Zodd2698/nodejsapp')
+            repositoryName('00juansierra00/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
